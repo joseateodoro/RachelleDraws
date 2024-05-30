ef76a3674c76f204a01a66d05b67deb7b52f7677
Jose Teodoro <joseateodoro.jt@gmail.com>
2024-05-30 15:44:38 -0400

    2024-05-30: updated page.js | new homepage.module.css | added componenents/images folder

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <div className="container">
+      <body className={inter.className}>
+        {/* <Navbar /> */}
+        {children}
+        <br />
+        </body>
+        </div>
     </html>
   );
 }
